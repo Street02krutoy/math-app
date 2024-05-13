b5d3612c218b6ab8be7cfd7ba8a9426704de1516
srite <masaanekdot@gmail.com>
2024-05-13 13:21:05 +0300

    INITIAL COMMENT

---
+++ lib/resources/pages/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:nylo_framework/nylo_framework.dart';
+import 'package:nylo_framework/theme/helper/ny_theme.dart';
+import 'package:flutter/material.dart';
+import '/bootstrap/extensions.dart';
+import '/resources/widgets/logo_widget.dart';
+import '/resources/widgets/safearea_widget.dart';
+import '/bootstrap/helpers.dart';
+import '/app/controllers/home_controller.dart';
+
+class HomePage extends NyStatefulWidget<HomeController> {
+  static const path = '/home';
+
+  HomePage({super.key}) : super(path, child: _HomePageState());
+}
+
+class _HomePageState extends NyState<HomePage> {
+  /// The boot method is called before the [view] is rendered.
+  /// You can override this method to perform any async operations.
+  /// Try uncommenting the code below.
+  // @override
+  // boot() async {
+  //   dump("boot");
+  //   await Future.delayed(Duration(seconds: 2));
+  // }
+
+  /// If you would like to use the Skeletonizer loader,
+  /// uncomment the code below.
+  // bool get useSkeletonizer => true;
+
+  /// The Loading widget is shown while the [boot] method is running.
+  /// You can override this method to show a custom loading widget.
+  // @override
+  // Widget loading(BuildContext context) {
+  //   return Scaffold(
+  //       body: Center(child: Text("Loading..."))
+  //   );
+  // }
+
+  /// The [view] method should display your page.
+  @override
+  Widget view(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("app_name".tr()),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: widget.controller.login,
+              icon: const Icon(Icons.login),
+            ),
+          ],
+        ),
+        body: Container());
+  }
+
+  bool get isThemeDark =>
+      ThemeProvider.controllerOf(context).currentThemeId ==
+      getEnv('DARK_THEME_ID');
+}
