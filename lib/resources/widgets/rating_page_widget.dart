fc5037caf35ef8016b1ddd28a69e85920f52407c
Alexey <bazooka2008@inbox.ru>
2024-05-22 22:39:21 +0300

    теперь топ3 это один класс

--- lib/resources/widgets/rating_page_widget.dart
+++ lib/resources/widgets/rating_page_widget.dart
@@ -13,6 +13,17 @@ class RatingPage extends StatefulWidget {
 class _RatingPageState extends NyState<RatingPage> {
   _RatingPageState() {}
 
+  List<Map> rating = [];
+
+  @override
+  void initState() {
+    for (int i = 1; i < 101; i++) {
+      rating.add({"position": i, "name": "User", "rating": 200 - i});
+    }
+    super.initState();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
@@ -21,30 +32,37 @@ class _RatingPageState extends NyState<RatingPage> {
       appBar: AppBar(
         title: Text("rating.page_name".tr()),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: Column(
         children: [
-          ProfileTop2(
-            name: "User",
-            child: Container(
-              color: theme.cardColor,
-            ),
-            rating: 58.2,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ProfileTop(
+                place: 2,
+                name: "User",
+                child: Container(
+                  color: theme.cardColor,
+                ),
+                rating: 58.2,
+              ),
+              ProfileTop(
+                place: 1,
+                name: "User",
+                child: Container(
+                  color: theme.cardColor,
+                ),
+                rating: 58.2,
+              ),
+              ProfileTop(
+                place: 3,
+                name: "User",
+                child: Container(
+                  color: theme.cardColor,
+                ),
+                rating: 58.2,
+              )
+            ],
           ),
-          ProfileTop1(
-            name: "User",
-            child: Container(
-              color: theme.cardColor,
-            ),
-            rating: 58.2,
-          ),
-          ProfileTop3(
-            name: "User",
-            child: Container(
-              color: theme.cardColor,
-            ),
-            rating: 58.2,
-          )
         ],
       ),
     );
--- lib/resources/widgets/top_image_widgets.dart
+++ lib/resources/widgets/top_image_widgets.dart
@@ -1,56 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/resources/widgets/round_image_widget.dart';
 
-class ProfileTop1 extends StatelessWidget {
-  const ProfileTop1({super.key, required this.child, required this.rating, required this.name});
+class ProfileTop extends StatelessWidget {
+  ProfileTop(
+      {super.key,
+      required this.child,
+      required this.rating,
+      required this.name,
+      required this.place});
 
   final String name;
   final Widget child;
   final num rating;
+  final int place;
+  late Color color;
+  late String path;
+  late double radius;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: RoundImage(
-                child: child,
-                roundColor: Color.fromRGBO(255, 199, 0, 1),
-                radius: 60,
-              ),
-            ),
-            Image.asset("public/assets/images/top1_icon.png"),
-          ],
-          alignment: AlignmentDirectional.bottomCenter,
-        ),
-        Text(
-          name,
-          textScaler: TextScaler.linear(2),
-        ),
-        Row(
-          children: [
-            Image.asset("public/assets/images/star.png", width: 25, height: 25,),
-            Padding(padding: EdgeInsets.only(right: 10),),
-            Text(rating.toString(), style: TextStyle(fontSize: 20,),),
-          ],
-        )
-      ],
-    );
+  void init() {
+    switch (place) {
+      case 1:
+        color = Color.fromRGBO(255, 199, 0, 1);
+        path = "public/assets/images/top1_icon.png";
+        radius = 60;
+        break;
+      case 2:
+        color = Color.fromRGBO(147, 147, 147, 1);
+        path = "public/assets/images/top2_icon.png";
+        radius = 50;
+        break;
+      case 3:
+        color = Color.fromRGBO(168, 100, 0, 1);
+        path = "public/assets/images/top3_icon.png";
+        radius = 50;
+        break;
+    }
   }
-}
-
-class ProfileTop2 extends StatelessWidget {
-  const ProfileTop2({super.key, required this.child, required this.rating, required this.name});
-
-  final String name;
-  final Widget child;
-  final num rating;
 
   @override
   Widget build(BuildContext context) {
+    init();
     return Column(
       children: [
         Stack(
@@ -59,11 +48,11 @@ class ProfileTop2 extends StatelessWidget {
               padding: EdgeInsets.only(bottom: 20),
               child: RoundImage(
                 child: child,
-                roundColor: Color.fromRGBO(147, 147, 147, 1),
-                radius: 50,
+                roundColor: color,
+                radius: radius,
               ),
             ),
-            Image.asset("public/assets/images/top2_icon.png"),
+            Image.asset(path),
           ],
           alignment: AlignmentDirectional.bottomCenter,
         ),
@@ -73,50 +62,20 @@ class ProfileTop2 extends StatelessWidget {
         ),
         Row(
           children: [
-            Image.asset("public/assets/images/star.png", width: 25, height: 25,),
-            Padding(padding: EdgeInsets.only(right: 10),),
-            Text(rating.toString(), style: TextStyle(fontSize: 20,),),
-          ],
-        )
-      ],
-    );
-  }
-}
-
-class ProfileTop3 extends StatelessWidget {
-  const ProfileTop3({super.key, required this.child, required this.rating, required this.name});
-
-  final String name;
-  final Widget child;
-  final num rating;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
+            Image.asset(
+              "public/assets/images/star.png",
+              width: 25,
+              height: 25,
+            ),
             Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: RoundImage(
-                child: child,
-                roundColor: Color.fromRGBO(168, 100, 0, 1),
-                radius: 50,
+              padding: EdgeInsets.only(right: 10),
+            ),
+            Text(
+              rating.toString(),
+              style: TextStyle(
+                fontSize: 20,
               ),
             ),
-            Image.asset("public/assets/images/top3_icon.png"),
-          ],
-          alignment: AlignmentDirectional.bottomCenter,
-        ),
-        Text(
-          name,
-          textScaler: TextScaler.linear(2),
-        ),
-        Row(
-          children: [
-            Image.asset("public/assets/images/star.png", width: 25, height: 25,),
-            Padding(padding: EdgeInsets.only(right: 10),),
-            Text(rating.toString(), style: TextStyle(fontSize: 20,),),
           ],
         )
       ],
