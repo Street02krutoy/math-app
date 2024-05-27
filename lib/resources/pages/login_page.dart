c6a298d5cb686e89b8b06982e1a5d8c3d86a2b57
srite <masaanekdot@gmail.com>
2024-05-27 14:42:14 +0300

    авторизация

---
+++ lib/resources/pages/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+class LoginPage extends NyStatefulWidget {
+  static const path = '/login';
+
+  LoginPage({super.key}) : super(path, child: _LoginPageState());
+}
+
+class _LoginPageState extends NyState<LoginPage> {
+  @override
+  init() async {}
+
+  /// Use boot if you need to load data before the [view] is rendered.
+  // @override
+  // boot() async {
+  //
+  // }
+
+  @override
+  Widget view(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("login.page_name".tr())),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [TextField(), TextField()],
+          ),
+        ),
+      ),
+    );
+  }
+}
